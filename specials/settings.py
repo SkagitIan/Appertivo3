412bfdd08251fbd0e543409bf6bcc2b6b5b45213
Ian <ian.larsen.1976@gmail.com>
2025-08-16 15:58:45 -0700

    udpate

--- specials/settings.py
+++ specials/settings.py
@@ -29,8 +29,10 @@ SECRET_KEY = 'django-insecure-9p1o@*d)+lij5nyyv6=8i*m^r_gmn&2(2ol-twbzpyf9#v&f4w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    "appertivo.com", "www.appertivo.com",
+    "app.appertivo.com", "localhost", "127.0.0.1"
+]
 
 # Application definition
 
