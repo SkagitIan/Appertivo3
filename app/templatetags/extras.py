ecc7f3e8308b2e5be478e61303dfd80208b81b75
Ian Larsen <ian.larsen.1976@gmail.com>
2025-09-08 16:48:32 +0000

    update

--- app/templatetags/extras.py
+++ app/templatetags/extras.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def getattribute(obj, name):
     return getattr(obj, name, "")
+
+
+@register.filter
+def to(value, end):
+    return range(value, end)
